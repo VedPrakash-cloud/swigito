6424ef8ff7c81b70e8d4a621c00687eda1a2ddb2
VedPrakash-cloud <rocky007ved@gmail.com>
2025-05-15 22:45:39 +0530

    Add files via upload
    updating the file

--- script.js
+++ script.js
@@ -27,23 +27,100 @@ function renderCart(){
     let cartContainer = document.querySelector('.cart-photo');
     cartContainer.innerHTML = '';
 
+
+//-- cart item list start here--//
     Object.values(cart).forEach(item =>{
         let itemElement = document.createElement('div');
-        itemElement.textContent = `${item.name}- ${item.quantity}`;
-        cartContainer.appendChild(itemElement)
+        itemElement.classList.add('cart-items');
+
+        let itemName = document.createElement('h6');
+        itemName.classList.add('item-cart');
+        itemName.textContent = `${item.name}`;
+
+        let itemCount = document.createElement('span');
+        itemCount.classList.add('item-count');
+        itemCount.textContent = `${item.quantity}x`;
+
+        let perItemPrice = document.createElement('span');
+        perItemPrice.classList.add('cart-price');
+        perItemPrice.textContent = `@ $${item.price.toFixed(2)}`;
+
+        let totalPrice = document.createElement('span');
+        totalPrice.classList.add('cart-cost');
+        totalPrice.textContent = `$${(item.price*item.quantity).toFixed(2)}`;
+
+        let crossButton = document.createElement('img');
+        crossButton.classList.add('cancel-item');
+        crossButton.src = `assets/images/icon-remove-item.svg`;
+        crossButton.alt = 'Cancel Item';
+        crossButton.onclick = ()=>{
+            itemElement.textContent = '';
+        }
+
+        itemElement.appendChild(itemName);
+        // itemElement.appendChild(document.createElement('br'));
+        itemElement.appendChild(itemCount);
+        itemElement.appendChild(perItemPrice);
+        itemElement.appendChild(totalPrice);
+        itemElement.appendChild(crossButton);
+
+        cartContainer.appendChild(itemElement);
+    });
+
+//-- total cost of order starts here --//
+    let total = 0;
+    Object.values(cart).forEach(item=>{
+        total += item.quantity*item.price;
     })
-}
+    let totalAmount= document.createElement('p');
+        totalAmount.classList.add('total-cost');
+        totalAmount.innerHTML = `<span>Order Total</span> <strong>$${total.toFixed(2)}</strong>`;
+
+
+//-- carbon confirm button starts here --//
+    let carbon = document.createElement('div');
+    carbon.classList.add('carbon-image');
+
+    let carbonNeutral = document.createElement('img');
+    carbonNeutral.classList.add('neutral');
+    carbonNeutral.src = `assets/images/icon-carbon-neutral.svg`
+    carbonNeutral.alt = 'carbon neutral';
+
+    let deliveryMessage = document.createElement('p');
+    deliveryMessage.classList.add('text-center','text');
+    deliveryMessage.innerHTML = 'This is a <strong>carbon-neutral</strong> delivery';
+
+    carbon.appendChild(carbonNeutral);
+    carbon.appendChild(deliveryMessage);
+
+        cartContainer.appendChild(totalAmount);
+        cartContainer.appendChild(carbon);
+
+    let confirm = document.createElement('button');
+    confirm.classList.add('confirm-button');
+    confirm.textContent = 'Confirm Order';
+    confirm.onclick=()=>{
+        if(total === 0){
+            alert('please add some items')
+        }else{
+            alert("Your Order is confirmed!!!");
+            alert(`Total amount paid $ ${total.toFixed(2)}`);
+        }
+    };
+
+    cartContainer.appendChild(confirm);
+
+};
 
 function updateCart(item, change){
     if(!cart[item.category]){
-        cart[item.category] = {name:item.name, quantity:0, price:item.price};
+        cart[item.category] = {name:item.name, price:item.price, quantity:0};
     }
     cart[item.category].quantity += change;
 
     if(cart[item.category].quantity < 1 && change > 0){
         cart[item.category].quantity = 1;
     }
-    console.log(cart[item.category].name, cart[item.category].quantity);
     updateCartCount();
     renderCart();
 }
@@ -56,24 +133,22 @@ function renderData(data){
         card.classList.add('col-4');
 
         let img = document.createElement('img');
-        img.classList.add('pic');
+        img.classList.add('img-fluid');
         img.src = item.image.tablet;
         img.alt = item.name;
 
         // Add to cart Button//
 
         let addButton = document.createElement('button');
-        addButton.classList.add('btn-light');
+        addButton.classList.add('btn','btn-light');
 
         let cartImg = document.createElement('img');
-        cartImg.classList.add('basket-icon');
+        cartImg.classList.add('img-fluid');
         cartImg.src = `assets/images/icon-add-to-cart.svg`;
         cartImg.alt = 'basket';
-        cartImg.style.width='20px';
-        cartImg.style.marginRight = '5px'
 
         let cartText = document.createElement('span');
-        cartText.classList.add('Cart1-text');
+        cartText.classList.add('text-center');
         cartText.textContent = 'Add to Cart';
 
         addButton.onclick = ()=>{
@@ -120,7 +195,7 @@ function renderData(data){
                 console.log("minus is clicked");
             }else{
                 if(!cart[item.category]){
-                    cart[item.category] = {name: item.name, quantity: 1, price: item.price};
+                    cart[item.category] = {name: item.name, quantity: 0, price: item.price};
                 }else{
                     count += 1;
                 }
